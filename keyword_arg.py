d71f9a54d4643ab060e493050ac6a34aded2443a
meheady <hmmehedi55@gmail.com>
2025-07-23 08:27:54 +0600

    keyword argument

--- keyword_arg.py
+++ keyword_arg.py
@@ -6,3 +6,16 @@ def my_func(fName, lName):
 result = my_func("Mehedi", "Hasan")
 
 print("Full name:", result)
+
+
+
+
+#def new_func(first, last):
+def new_func(**kwargs):
+    return kwargs
+    print("First name:", first)
+    print("Last name:", last)
+    return f"{first} {last}"
+
+data  = new_func(first="Mehedi", last="Hasan")
+print("Full name with keyword args:", data)
